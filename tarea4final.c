770c4ae1c95a83af00df2d2817f41aded3543ea3
Bastian Escalona Morales <bastianescalona@hotmail.com>
2015-06-09 21:12:21 -0400

    Tarea4 Final
    Ahora si xd

--- tarea4final.c
+++ tarea4final.c
@@ -5,14 +5,14 @@
 int main(){
 	char array[N];
         int i=0,j=0,k=0;
-        for(i=0;i<N;i++) array[i]=0; //Recorre arreglo llenandolo con 0
+        for(i=0;i<N;i++) array[i]=0; //Recorre arreglo llenandolo con 0.
         printf("Ingrese la palabra\n");
         scanf("%s",array); //Pide la palabra por teclado.
         char temp[N];
-        for(i=0;i<N;i++) temp[i]=0; //Recorre arreglo llenandolo con 0
+        for(i=0;i<N;i++) temp[i]=0; //Recorre arreglo llenandolo con 0.
         int cont = 0;
 	int aux[N];
-        for(i=0;i<N;i++) aux[i]=0; //Recorre arreglo llenandolo con 0
+        for(i=0;i<N;i++) aux[i]=0; //Recorre arreglo llenandolo con 0.
 	while(j<N){
                 aux[j] = 0;
 		cont=0;
@@ -30,7 +30,7 @@ int main(){
         cont = 0;
         for(i=0;i<N;i++) if( aux[i]%2 == 1 ) cont++; //Se ve si es palindromo o no, si lo es arrojara un 1.
         if (cont <= 3) cont = 1;
-        else cont = 0;
+        else cont = 0; // De lo contrario arrojara un 0.
 	printf("%d\n", cont );
 	return 0;
 }
